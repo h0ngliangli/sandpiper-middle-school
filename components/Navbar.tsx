90d174faa5e08dbdb896811df710003d6aca3f00
Hongliang Li <woodyli1982@gmail.com>
2026-03-13 16:16:47 -0700

    navbar: css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useState, useEffect, useRef } from 'react';
 import { Menu, X } from 'lucide-react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -44,61 +44,59 @@ const Navbar: React.FC = () => {
       className={`fixed left-1/2 top-0 z-50 w-full max-w-6xl -translate-x-1/2
         bg-sandpiper-blue transition-transform duration-300 ${
           isVisible ? 'translate-y-0' : '-translate-y-full'
-        }`}
+        } px-4`}
     >
-      <div className="mx-auto max-w-6xl px-4">
-        <div className="flex h-16 items-center justify-between">
-          <div className="flex flex-grow items-center gap-2">
-            <button
-              onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-              aria-label="Back to top"
-            >
-              <img
-                src="/favicon.svg"
-                className="h-10 w-10"
-                alt="Sandpiper Middle School logo"
-              />
-            </button>
-            <div
-              className="flex-grow whitespace-nowrap text-center tracking-wider
-                md:text-xl lg:text-left"
-            >
-              SANDPIPER MIDDLE SCHOOL
-            </div>
+      <div className="flex h-16 items-center justify-between">
+        <div className="flex flex-grow items-center gap-2">
+          <button
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+            aria-label="Back to top"
+          >
+            <img
+              src="/favicon.svg"
+              className="h-10 w-10"
+              alt="Sandpiper Middle School logo"
+            />
+          </button>
+          <div
+            className="flex-grow whitespace-nowrap text-center tracking-wider
+              md:text-xl lg:text-left"
+          >
+            SANDPIPER MIDDLE SCHOOL
           </div>
+        </div>
 
-          <div className="hidden lg:block">
-            <div className="flex items-baseline">
-              {navLinks.map((link) => (
-                <a
-                  key={link.name}
-                  href={link.href}
-                  className="px-2 py-2 text-sm font-semibold transition-all
-                    duration-300 hover:text-sandpiper-gold hover:underline"
-                >
-                  {link.name}
-                </a>
-              ))}
-            </div>
+        <div className="hidden lg:block">
+          <div className="flex items-baseline">
+            {navLinks.map((link) => (
+              <a
+                key={link.name}
+                href={link.href}
+                className="px-2 py-2 text-sm font-semibold transition-all
+                  duration-300 hover:text-sandpiper-gold hover:underline"
+              >
+                {link.name}
+              </a>
+            ))}
           </div>
+        </div>
 
-          <div className="-mr-2 flex lg:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              type="button"
-              className="inline-flex items-center justify-center rounded-md
-                focus:outline-none"
-              aria-controls="mobile-menu"
-              aria-expanded={isOpen}
-            >
-              <span className="sr-only">Open main menu</span>
-              {isOpen ? (
-                <X className="block h-6 w-6" />
-              ) : (
-                <Menu className="block h-6 w-6" />
-              )}
-            </button>
-          </div>
+        <div className="-mr-2 flex lg:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            type="button"
+            className="inline-flex items-center justify-center rounded-md
+              focus:outline-none"
+            aria-controls="mobile-menu"
+            aria-expanded={isOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            {isOpen ? (
+              <X className="block h-6 w-6" />
+            ) : (
+              <Menu className="block h-6 w-6" />
+            )}
+          </button>
         </div>
       </div>
 
