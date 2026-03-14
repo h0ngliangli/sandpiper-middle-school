465d50204778b01df83347504656d46ee1430ce6
Hongliang Li <woodyli1982@gmail.com>
2026-03-13 21:36:51 -0700

    chatbot: detect virtual keyboard and display chatbot on top (if available)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const REDIRECT_SIGNAL = 'REDIRECT_TO_WHATSAPP';
+export const REDIRECT_SIGNAL = 'REDIRECT_TO_PARENT_AMBASSADORS';
 
 export const GEMINI_URL =
   'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
